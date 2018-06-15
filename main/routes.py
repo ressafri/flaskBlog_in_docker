f473b9898ac0702d17361fddffede5da40b2c96a
redwan <redwan@gmx.com>
2018-06-15 01:09:19 +0100

    02 templates -- almost done

--- main/routes.py
+++ main/routes.py
@@ -1,14 +1,30 @@
+from flask import Flask, render_template
 from app import app
+
+posts = [
+    {
+        'author': 'redwan essafri',
+        'title': 'Blog post 1',
+        'content': 'fist post content',
+        'date_posted': 'May 15, 2018'
+    },
+
+    {
+        'author': 'Mariam Essafri',
+        'title': 'Blog post 2',
+        'content': 'fist post content',
+        'date_posted': 'May 14, 2018'
+    }
+
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>This is the home page</h1>"
-
-# app.config.from_object('settings')
+    return render_template('home.html', posts=posts, title='home')
 
 
 @app.route("/about")
 def about():
-    return "<h1>about me</h1>"
+    return render_template('about.html')
